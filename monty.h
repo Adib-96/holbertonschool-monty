95ceb5a4926bf37aae03b77b9d0c60c24aff3795
Adib-96 <adibbh1996@gmail.com>
2023-11-23 23:04:46 +0100

    add header file

--- monty.h
+++ monty.h
@@ -1,7 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
 #include <stdio.h>
-
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -13,9 +17,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+int n;
+struct stack_s *prev;
+struct stack_s *next;
 } stack_t;
 
 /**
@@ -28,7 +32,20 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+extern char *value;
+int main(int argc, char *argv[]);
+int is_int(char *value);
+void free_stack(stack_t *stack);
+void execute_opcode(stack_t **stack, unsigned int line_number, char *opcode);
+void push(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 #endif
